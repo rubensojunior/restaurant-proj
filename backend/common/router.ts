f795882b22d764c7efd120e9518224f87c976571
jroliveirati <jroliveirati@gmail.com>
2019-11-28 10:51:35 -0300

    Refactoring model-routers

--- backend/common/router.ts
+++ backend/common/router.ts
@@ -5,15 +5,28 @@ import {NotFoundError} from 'restify-errors'
 export abstract class Router extends EventEmitter {
     abstract applyRoutes(application: restify.Server)
 
-        render(response: restify.Response, next: restify.Next){
-            return (document)=>{
-                if(document){
-                    this.emit('beforeRender', document)
-                    response.json(document)
-                }else{
-                    throw new NotFoundError('Documento não encontrado')
-                }
-                return next()
+    render(response: restify.Response, next: restify.Next){
+        return (document)=>{
+            if(document){
+                this.emit('beforeRender', document)
+                response.json(document)
+            }else{
+                throw new NotFoundError('Documento não encontrado')
             }
+            return next()
+        }
+    }
+
+    renderAll(response: restify.Response, next: restify.Next){
+        return (documents: any[])=>{
+            if(documents){
+                documents.forEach(document=>{
+                    this.emit('beforeRender',document)
+                })
+                response.json(documents)
+            }else{
+                response.json([])
+            }
+        }
     }
 }

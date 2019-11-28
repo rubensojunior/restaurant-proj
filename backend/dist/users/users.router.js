e65a90bee92174c5b0513f8988244110bc171b62
jroliveirati <jroliveirati@gmail.com>
2019-11-27 22:27:23 -0300

    Refactoring routes

--- backend/dist/users/users.router.js
+++ backend/dist/users/users.router.js
@@ -3,30 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const router_1 = require("../common/router");
 const users_model_1 = require("./users.model");
 class UsersRouter extends router_1.Router {
+    constructor() {
+        super();
+        this.on('beforeRender', document => {
+            document.password = undefined;
+        });
+    }
     applyRoutes(application) {
         application.get('/users', (req, resp, next) => {
-            users_model_1.User.find().then(users => {
-                resp.json(users);
-                return next();
-            });
+            users_model_1.User.find().then(this.render(resp, next));
         });
         application.get('/users/:id', (req, resp, next) => {
-            users_model_1.User.findById(req.params.id).then(user => {
-                if (user) {
-                    resp.json(user);
-                    return next();
-                }
-                resp.send(404);
-                return next();
-            });
+            users_model_1.User.findById(req.params.id).then(this.render(resp, next));
         });
         application.post('/users', (req, resp, next) => {
             let user = new users_model_1.User(req.body);
-            user.save().then(user => {
-                user.password = undefined;
-                resp.json(user);
-                return next();
-            });
+            user.save().then(this.render(resp, next));
         });
         application.put('/users/:id', (req, resp, next) => {
             users_model_1.User.replaceOne({ _id: req.params.id }, req.body)
@@ -37,10 +29,7 @@ class UsersRouter extends router_1.Router {
                 else {
                     resp.send(404);
                 }
-            }).then(user => {
-                resp.json(user);
-                return next();
-            });
+            }).then(this.render(resp, next));
         });
         application.del('/users/:id', (req, resp, next) => {
             users_model_1.User.deleteOne({ _id: req.params.id })
--- backend/users/users.router.ts
+++ backend/users/users.router.ts
@@ -3,32 +3,26 @@ import * as restify from 'restify'
 import {User} from './users.model'
 
 class UsersRouter extends Router {
+
+    constructor(){
+        super()
+        this.on('beforeRender', document=>{
+            document.password = undefined
+        })
+    }
+
     applyRoutes(application: restify.Server){
         application.get('/users', (req, resp, next)=>{
-            User.find().then(users=>{
-                resp.json(users)
-                return next()
-            })
+            User.find().then(this.render(resp,next))
         })
 
         application.get('/users/:id', (req, resp, next)=>{
-            User.findById(req.params.id).then(user=>{
-                if(user){
-                    resp.json(user)
-                    return next()
-                }
-                resp.send(404)
-                return next()
-            })
+            User.findById(req.params.id).then(this.render(resp,next))
         })
 
         application.post('/users', (req, resp, next)=>{
             let user = new User(req.body)
-            user.save().then(user=>{
-                user.password = undefined
-                resp.json(user)
-                return next()
-            })
+            user.save().then(this.render(resp,next))
         })
 
         application.put('/users/:id', (req, resp, next)=>{
@@ -39,10 +33,7 @@ class UsersRouter extends Router {
                     }else{
                         resp.send(404)
                     }
-            }).then(user=>{
-                resp.json(user)
-                return next()
-            })
+            }).then(this.render(resp,next))
         })
 
         application.del('/users/:id',(req, resp, next)=>{
